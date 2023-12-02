4b9389491eb5554cdeffb2c0dae5c0065dd8987c
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-02 15:42:57 +0200

    Add Assets Images -Flutter Assets Gen

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,19 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesOnBoardingOne
+  /// assets/images/on_boarding_one.webp
+  static const String onBoardingOne = "assets/images/on_boarding_one.webp";
+
+  /// Assets for assetsImagesOnBoardingThree
+  /// assets/images/on_boarding_three.png
+  static const String onBoardingThree = "assets/images/on_boarding_three.png";
+
+  /// Assets for assetsImagesOnBoardingTwo
+  /// assets/images/on_boarding_Two.webp
+  static const String onBoardingTwo = "assets/images/on_boarding_Two.webp";
+
+  /// Assets for assetsImagesSplashLogo
+  /// assets/images/Splash_logo.webp
+  static const String splashLogo = "assets/images/Splash_logo.webp";
+}

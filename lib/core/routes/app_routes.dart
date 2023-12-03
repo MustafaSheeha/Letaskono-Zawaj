98ac51b9a6d27ac84762e86b68509150e3721ae1
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-03 11:40:23 +0200

    Remove go route

--- lib/core/routes/app_routes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'package:letaskono_zawaj/features/splash/presentation/view/splash_view.dart';
-
-final GoRouter goRouter=GoRouter(routes: [
-  GoRoute(path: '/',
-  builder: (context, state) => const SplashView(),)
-]);

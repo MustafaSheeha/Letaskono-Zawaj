b1c492012ff405c5e5e29e1456616075b4858201
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-03 13:39:02 +0200

    Create CustomElevatedButton

--- lib/features/on_boarding/presentation/view/on_boarding.dart
+++ lib/features/on_boarding/presentation/view/on_boarding.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:letaskono_zawaj/core/utils/app_assets.dart';
-import 'package:letaskono_zawaj/core/utils/app_colors.dart';
 import 'package:letaskono_zawaj/core/utils/app_strings.dart';
-import 'package:letaskono_zawaj/core/utils/app_text_styles.dart';
-import 'package:letaskono_zawaj/core/widgets/custom_text_button.dart';
-import 'package:letaskono_zawaj/features/on_boarding/data/on_boarding_list.dart';
+import 'package:letaskono_zawaj/core/widgets/custom_elevated_button.dart';
 import 'package:letaskono_zawaj/features/on_boarding/presentation/view/widgets/on_boarding_body.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:letaskono_zawaj/features/on_boarding/presentation/view/widgets/skip_text_widget.dart';
 
 class OnBoardingView extends StatelessWidget {
   OnBoardingView({super.key});
@@ -22,21 +18,12 @@ class OnBoardingView extends StatelessWidget {
           child: ListView(
             physics: const BouncingScrollPhysics(),
             children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CustomTextButton(
-                    text: AppStrings.skip, fontSize: 0.06 * screenWidth),
-              ),
+              SkipTextWidget(screenWidth: screenWidth),
               OnBoardingBody(controller: controller),
-              SizedBox(height: 0.05 * screenHeight),
-              ElevatedButton(
+              CustomElevatedButton(
+                text: AppStrings.next,
                 onPressed: () {},
-                child: Text(
-                  AppStrings.next,
-                  style: AppTextStyles.cairoW400White
-                      .copyWith(fontSize: 0.05 * screenWidth),
-                ),
-              )
+              ),
             ],
           ),
         ),

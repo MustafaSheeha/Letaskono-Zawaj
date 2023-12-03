66aad75529fe574fadf34c76211141efb7c1b2e7
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-03 09:54:53 +0200

    Create AppTextStyles

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:letaskono_zawaj/core/utils/app_colors.dart';
+
+class AppTextStyles {
+  static const cairoW400PrimaryColor = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
+  static const cairoW400Grey = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+  );
+  static const cairoW400White = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+}

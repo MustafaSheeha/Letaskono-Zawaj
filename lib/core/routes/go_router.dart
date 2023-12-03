978a7a99df2d86208cb5165977222540944fa941
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-03 11:45:13 +0200

    Create MaterialApp route map

--- lib/core/routes/go_router.dart
+++ lib/core/routes/go_router.dart
@@ -1,12 +1,12 @@
-import 'package:go_router/go_router.dart';
-import 'package:letaskono_zawaj/core/routes/app_routes.dart';
-import 'package:letaskono_zawaj/features/on_boarding/presentation/view/on_boarding.dart';
-import 'package:letaskono_zawaj/features/splash/presentation/view/splash_view.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:letaskono_zawaj/core/routes/app_routes.dart';
+// import 'package:letaskono_zawaj/features/on_boarding/presentation/view/on_boarding.dart';
+// import 'package:letaskono_zawaj/features/splash/presentation/view/splash_view.dart';
 
-final GoRouter goRouter=GoRouter(routes: [
-  GoRoute(path: AppRoutes.splash,
-  builder: (context, state) => const SplashView()),
-  GoRoute(path: AppRoutes.onBoarding,
-  builder: (context, state) => const OnBoardingView()),
-]);
+// final GoRouter goRouter=GoRouter(routes: [
+//   GoRoute(path: AppRoutes.splash,
+//   builder: (context, state) => const SplashView()),
+//   GoRoute(path: AppRoutes.onBoarding,
+//   builder: (context, state) => const OnBoardingView()),
+// ]);
 
--- lib/material_app.dart
+++ lib/material_app.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:letaskono_zawaj/core/routes/app_routes.dart';
 import 'package:letaskono_zawaj/core/utils/app_theme_data.dart';
+import 'package:letaskono_zawaj/features/on_boarding/presentation/view/on_boarding.dart';
+import 'package:letaskono_zawaj/features/splash/presentation/view/splash_view.dart';
 
 class LetaskonoZawajApp extends StatelessWidget {
   const LetaskonoZawajApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
-      routerConfig: goRouter,
+    return  MaterialApp(
+      routes: {
+        AppRoutes.splash:(context) => SplashView(),
+        AppRoutes.onBoarding:(context) => OnBoardingView(),
+      },
+      home: SplashView(),
       theme: AppThemeData().theme(),
     );
   }

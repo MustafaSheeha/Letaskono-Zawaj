927e01f6f34d10fad1a5dfcfb20884323a76a793
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-04 19:56:59 +0200

    Move BlocProvider AuthCubit from MaterialApp to RegisterView

--- lib/features/auth/presentation/view/register/register_view.dart
+++ lib/features/auth/presentation/view/register/register_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:letaskono_zawaj/core/utils/app_strings.dart';
 import 'package:letaskono_zawaj/core/utils/app_text_styles.dart';
 import 'package:letaskono_zawaj/core/widgets/custom_app_bar.dart';
 import 'package:letaskono_zawaj/core/widgets/custom_text_button.dart';
+import 'package:letaskono_zawaj/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:letaskono_zawaj/features/auth/presentation/view/register/widgets/registeration_form.dart';
 import 'package:letaskono_zawaj/features/auth/presentation/view/widgets/auth_container.dart';
 
@@ -13,30 +15,33 @@ class RegisterView extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: SingleChildScrollView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        child: Column(
-          children: [
-            const CustomAppBar(),
-            SizedBox(height: 0.05 * screenHeight),
-            AuthContainer(
-              widget: Column(children: [
-                Text(AppStrings.welcome,
-                    style: AppTextStyles.cairoW800PrimaryColor
-                        .copyWith(fontSize: 0.08 * screenWidth)),
-                Text(AppStrings.createNewAccountToInteract,
-                    style: AppTextStyles.cairoW400Grey
-                        .copyWith(fontSize: 0.035 * screenWidth)),
-                RegisterationForm(),
-                CustomTextButton(
-                  text: AppStrings.alreadyHaveAccount,
-                  fontSize: 0.031 * screenWidth,
-                  onPressed: () {},
-                )
-              ]),
-            ),
-          ],
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          child: Column(
+            children: [
+              const CustomAppBar(),
+              SizedBox(height: 0.05 * screenHeight),
+              AuthContainer(
+                widget: Column(children: [
+                  Text(AppStrings.welcome,
+                      style: AppTextStyles.cairoW800PrimaryColor
+                          .copyWith(fontSize: 0.08 * screenWidth)),
+                  Text(AppStrings.createNewAccountToInteract,
+                      style: AppTextStyles.cairoW400Grey
+                          .copyWith(fontSize: 0.035 * screenWidth)),
+                  RegisterationForm(),
+                  CustomTextButton(
+                    text: AppStrings.alreadyHaveAccount,
+                    fontSize: 0.031 * screenWidth,
+                    onPressed: () {},
+                  )
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/material_app.dart
+++ lib/material_app.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:letaskono_zawaj/core/routes/app_routes.dart';
 import 'package:letaskono_zawaj/core/utils/app_theme_data.dart';
 import 'package:letaskono_zawaj/core/widgets/custom_persistant_bottom_nav_bar.dart';
-import 'package:letaskono_zawaj/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:letaskono_zawaj/features/auth/presentation/view/login/login_view.dart';
 import 'package:letaskono_zawaj/features/auth/presentation/view/register/register_view.dart';
 import 'package:letaskono_zawaj/features/on_boarding/presentation/view/on_boarding.dart';
@@ -21,10 +19,7 @@ class LetaskonoZawajApp extends StatelessWidget {
         AppRoutes.onBoarding: (context) => const OnBoardingView(),
         AppRoutes.customPersistantBottomNavBar: (context) =>
             CustomPersistantBottomNavBar(),
-        AppRoutes.register: (context) => BlocProvider(
-              create: (context) => AuthCubit(),
-              child: const RegisterView(),
-            ),
+        AppRoutes.register: (context) => const RegisterView(),
         AppRoutes.login: (context) => const LoginView(),
       },
       theme: AppThemeData().theme(),

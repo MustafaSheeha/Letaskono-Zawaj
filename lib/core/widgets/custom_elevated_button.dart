71ff82902fae40b0b4edc018af5f0e20131d24c7
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-04 20:08:16 +0200

    CustomElevatedButton add backgroundColor

--- lib/core/widgets/custom_elevated_button.dart
+++ lib/core/widgets/custom_elevated_button.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:letaskono_zawaj/core/utils/app_colors.dart';
 import 'package:letaskono_zawaj/core/utils/app_text_styles.dart';
 
 class CustomElevatedButton extends StatelessWidget {
   const CustomElevatedButton({
     super.key,
     required this.onPressed,
-    required this.text,
+    required this.text, this.backgroundColor,
   });
   final String text;
   final void Function() onPressed;
+  final Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     // final screenHeight = MediaQuery.of(context).size.height;
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor??AppColors.primaryColor,
+      ),
       onPressed: onPressed,
       child: Text(
         text,

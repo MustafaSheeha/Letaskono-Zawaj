98c5e5be849a3a969047c16b91b1c5dd0a75687f
Mustafa Sheeha <dev.mustafa.sheha@gmail.com>
2023-12-04 20:13:32 +0200

    finish WhoAreYouWidget

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const primaryColor = Color(0xff406882);
   static const scaffoldBackgroundColor = Color(0xfffefeff);
-  static const white = Color(0xffffffff);
-  static const grey = Color(0xff9da2af);
+  static const white = Colors.white;
+  static const grey = Colors.grey;
   static const red = Color(0xffFF0000);
   static const transparent = Colors.transparent;
   static const black = Colors.black;
--- lib/features/auth/presentation/view/register/widgets/who_are_you_widget.dart
+++ lib/features/auth/presentation/view/register/widgets/who_are_you_widget.dart
@@ -1,27 +1,54 @@
-
 import 'package:flutter/material.dart';
+import 'package:letaskono_zawaj/core/utils/app_colors.dart';
 import 'package:letaskono_zawaj/core/utils/app_strings.dart';
 import 'package:letaskono_zawaj/core/utils/app_text_styles.dart';
 import 'package:letaskono_zawaj/core/widgets/custom_elevated_button.dart';
 
-class WhoAreYouWidget extends StatelessWidget {
+class WhoAreYouWidget extends StatefulWidget {
   const WhoAreYouWidget({
     super.key,
   });
 
+  @override
+  State<WhoAreYouWidget> createState() => _WhoAreYouWidgetState();
+}
 
+class _WhoAreYouWidgetState extends State<WhoAreYouWidget> {
+  Color manButtonColor = AppColors.grey;
+  Color manTextColor = AppColors.primaryColor;
+  Color womanButtonColor = AppColors.grey;
+  Color womanTextColor = AppColors.primaryColor;
   @override
   Widget build(BuildContext context) {
-    
     final screenWidth = MediaQuery.of(context).size.width;
     // final screenHeight = MediaQuery.of(context).size.height;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         CustomElevatedButton(
-            onPressed: () {}, text: AppStrings.iamFemale),
+            onPressed: () {
+              setState(() {
+                womanButtonColor = AppColors.primaryColor;
+                womanTextColor = AppColors.white;
+
+                manButtonColor = AppColors.grey;
+                manTextColor = AppColors.primaryColor;
+              });
+            },
+            text: AppStrings.iamFemale,
+            backgroundColor: womanButtonColor),
         CustomElevatedButton(
-            onPressed: () {}, text: AppStrings.iamMale),
+            onPressed: () {
+              setState(() {
+                manButtonColor = AppColors.primaryColor;
+                manTextColor = AppColors.white;
+
+                womanButtonColor = AppColors.grey;
+                womanTextColor = AppColors.primaryColor;
+              });
+            },
+            text: AppStrings.iamMale,
+            backgroundColor: manButtonColor),
         Text(AppStrings.whoAreYou,
             style: AppTextStyles.cairoW300PrimaryColor
                 .copyWith(fontSize: 0.035 * screenWidth)),
